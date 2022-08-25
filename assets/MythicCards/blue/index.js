3c23690730d6d2719ca4e9d6face7269a4044d35
idanigor <idanigor@gmail.com>
2022-08-25 22:56:54 +0600

    fix: link myth2

--- assets/MythicCards/blue/index.js
+++ assets/MythicCards/blue/index.js
@@ -12,18 +12,18 @@
 // import blue12 from './blue12.webp';
 
 const cards = {
-	blue1: '/assets/MythicCards/blue/blue1.webp',
-	blue2: '/assets/MythicCards/blue/blue2.webp',
-	blue3: '/assets/MythicCards/blue/blue3.webp',
-	blue4: '/assets/MythicCards/blue/blue4.webp',
-	blue5: '/assets/MythicCards/blue/blue5.webp',
-	blue6: '/assets/MythicCards/blue/blue6.webp',
-	blue7: '/assets/MythicCards/blue/blue7.webp',
-	blue8: '/assets/MythicCards/blue/blue8.webp',
-	blue9: '/assets/MythicCards/blue/blue9.webp',
-	blue10: '/assets/MythicCards/blue/blue10.webp',
-	blue11: '/assets/MythicCards/blue/blue11.webp',
-	blue12: '/assets/MythicCards/blue/blue12.webp',
+	blue1: 'assets/MythicCards/blue/blue1.webp',
+	blue2: 'assets/MythicCards/blue/blue2.webp',
+	blue3: 'assets/MythicCards/blue/blue3.webp',
+	blue4: 'assets/MythicCards/blue/blue4.webp',
+	blue5: 'assets/MythicCards/blue/blue5.webp',
+	blue6: 'assets/MythicCards/blue/blue6.webp',
+	blue7: 'assets/MythicCards/blue/blue7.webp',
+	blue8: 'assets/MythicCards/blue/blue8.webp',
+	blue9: 'assets/MythicCards/blue/blue9.webp',
+	blue10: 'assets/MythicCards/blue/blue10.webp',
+	blue11: 'assets/MythicCards/blue/blue11.webp',
+	blue12: 'assets/MythicCards/blue/blue12.webp',
 }
 
 export default cards
